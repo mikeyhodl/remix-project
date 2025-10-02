871ddb09752e0a9316e1e8e9fca2ec2904da6fd4
Aniket-Engg <aniketengg.225@gmail.com>
2025-10-02 14:45:20 +0530

    no web3 in blocks ut

--- libs/remix-simulator/test/blocks.ts
+++ libs/remix-simulator/test/blocks.ts
@@ -1,7 +1,5 @@
 /* global describe, before, it */
-import { Web3 } from 'web3'
 import { Provider } from '../src/index'
-const web3 = new Web3()
 import * as assert from 'assert'
 import { ethers, BrowserProvider } from "ethers"
 
@@ -13,7 +11,6 @@ describe('blocks', () => {
       coinbase: '0x0000000000000000000000000000000000000001'
     })
     await provider.init()
-    web3.setProvider(provider as any)
     ethersProvider = new ethers.BrowserProvider(provider as any)
   })
 
@@ -203,26 +200,30 @@ describe('blocks', () => {
       ] as const
 
       const code = '0x608060405234801561001057600080fd5b506040516020806102018339810180604052602081101561003057600080fd5b810190808051906020019092919050505080600081905550506101a9806100586000396000f3fe60806040526004361061005c576000357c0100000000000000000000000000000000000000000000000000000000900480632a1afcd91461006157806360fe47b11461008c5780636d4ce63c146100c7578063ce01e1ec146100f2575b600080fd5b34801561006d57600080fd5b5061007661012d565b6040518082815260200191505060405180910390f35b34801561009857600080fd5b506100c5600480360360208110156100af57600080fd5b8101908080359060200190929190505050610133565b005b3480156100d357600080fd5b506100dc61013d565b6040518082815260200191505060405180910390f35b3480156100fe57600080fd5b5061012b6004803603602081101561011557600080fd5b8101908080359060200190929190505050610146565b005b60005481565b8060008190555050565b60008054905090565b80600081905550807f63a242a632efe33c0e210e04e4173612a17efa4f16aa4890bc7e46caece80de060405160405180910390a25056fea165627a7a7230582063160eb16dc361092a85ced1a773eed0b63738b83bea1e1c51cf066fa90e135d0029'
+      const signer = await ethersProvider.getSigner();
+      const contract = new ethers.ContractFactory(abi, code, signer)
 
-      const contract = new web3.eth.Contract(abi)
-      const accounts = await web3.eth.getAccounts()
+      const contractInstance = await contract.deploy(100)
+      await contractInstance.waitForDeployment()
 
-      const contractInstance: any = await contract.deploy({ data: code, arguments: [100]}).send({ from: accounts[0], gas: '400000' })
-      contractInstance.currentProvider = web3.eth.currentProvider
-      // contractInstance.givenProvider = web3.eth.currentProvider
+      const contractAddress = await contractInstance.getAddress()
+      const contractInteract = new ethers.Contract(contractAddress, abi, signer)
 
-      await contractInstance.methods.set(100).send({ from: accounts[0].toLowerCase(), gas: 400000 })
-      let storage = await web3.eth.getStorageAt(contractInstance.options.address, 0)
+      let tx = await contractInteract.set(100)
+      await tx.wait()
+      let storage = await ethersProvider.getStorage(contractAddress, "0x0")
       assert.deepEqual(storage, '0x64')
 
-      await contractInstance.methods.set(200).send({ from: accounts[0].toLowerCase(), gas: 400000 })
-      storage = await web3.eth.getStorageAt(contractInstance.options.address, 0)
+      tx = await contractInteract.set(200)
+      await tx.wait()
+      storage = await ethersProvider.getStorage(contractAddress, "0x0")
       assert.deepEqual(storage, '0xc8')
 
-      await contractInstance.methods.set(1).send({ from: accounts[0].toLowerCase(), gas: 400000 })
-      storage = await web3.eth.getStorageAt(contractInstance.options.address, 0)
+      tx = await contractInteract.set(1)
+      await tx.wait()
+      storage = await ethersProvider.getStorage(contractAddress, "0x0")
       assert.deepEqual(storage, '0x01')
-    })
+    }).timeout(20000)
   })
   describe('eth_call', () => {
     it('should get a value', async () => {
@@ -310,14 +311,18 @@ describe('blocks', () => {
 
       const code = '0x608060405234801561001057600080fd5b506040516020806102018339810180604052602081101561003057600080fd5b810190808051906020019092919050505080600081905550506101a9806100586000396000f3fe60806040526004361061005c576000357c0100000000000000000000000000000000000000000000000000000000900480632a1afcd91461006157806360fe47b11461008c5780636d4ce63c146100c7578063ce01e1ec146100f2575b600080fd5b34801561006d57600080fd5b5061007661012d565b6040518082815260200191505060405180910390f35b34801561009857600080fd5b506100c5600480360360208110156100af57600080fd5b8101908080359060200190929190505050610133565b005b3480156100d357600080fd5b506100dc61013d565b6040518082815260200191505060405180910390f35b3480156100fe57600080fd5b5061012b6004803603602081101561011557600080fd5b8101908080359060200190929190505050610146565b005b60005481565b8060008190555050565b60008054905090565b80600081905550807f63a242a632efe33c0e210e04e4173612a17efa4f16aa4890bc7e46caece80de060405160405180910390a25056fea165627a7a7230582063160eb16dc361092a85ced1a773eed0b63738b83bea1e1c51cf066fa90e135d0029'
 
-      const contract = new web3.eth.Contract(abi)
-      const accounts = await web3.eth.getAccounts()
+      const signer = await ethersProvider.getSigner();
+      const contract = new ethers.ContractFactory(abi, code, signer)
 
-      const contractInstance: any = await contract.deploy({ data: code, arguments: [100]}).send({ from: accounts[0], gas: '400000' })
-      contractInstance.currentProvider = web3.eth.currentProvider
+      const contractInstance = await contract.deploy(100)
+      await contractInstance.waitForDeployment()
 
-      const value = await contractInstance.methods.get().call({ from: accounts[0] })
+      const contractAddress = await contractInstance.getAddress()
+      const contractInteract = new ethers.Contract(contractAddress, abi, signer)
+
+      let value = await contractInteract.get()
+
       assert.deepEqual(value, 100)
-    })
+    }).timeout(5000)
   })
 })
