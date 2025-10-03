f20341fd99bb3535b53910d5e4a846c9625f4077
Aniket-Engg <aniketengg.225@gmail.com>
2025-10-03 20:15:21 +0530

    no web3-utils in remix-simulator

--- libs/remix-simulator/src/methods/accounts.ts
+++ libs/remix-simulator/src/methods/accounts.ts
@@ -1,7 +1,7 @@
 import { signTypedData, SignTypedDataVersion, TypedMessage, MessageTypes } from '@metamask/eth-sig-util'
 import { privateToAddress, toChecksumAddress, isValidPrivate, createAddressFromString, toBytes, bytesToHex, Account, intToHex } from '@ethereumjs/util'
 import type { PrefixedHexString } from '@ethereumjs/util'
-import { privateKeyToAccount } from 'web3-eth-accounts'
+import { ethers } from 'ethers'
 import * as crypto from 'crypto'
 
 type AccountType = {
@@ -121,11 +121,11 @@ export class Web3Accounts {
     if (!privateKey) {
       return cb(new Error('unknown account'))
     }
-    const account = privateKeyToAccount(privateKey as string)
+    const account = new ethers.Wallet(privateKey as string)
 
-    const data = account.sign(message)
-
-    cb(null, data.signature)
+    account.signMessage(message).then((signature) => {
+      cb(null, signature)
+    })
   }
 
   eth_chainId (_payload, cb) {
