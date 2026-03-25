3192daf98356aed64311ec0d982b7b084558861b
Joseph Izang <josephizang@gmail.com>
2026-03-25 11:34:52 +0100

    fix layout e2e

--- apps/remix-ide-e2e/src/tests/layout.test.ts
+++ apps/remix-ide-e2e/src/tests/layout.test.ts
@@ -24,7 +24,8 @@ module.exports = {
       .click('[data-id="movePluginToLeft"]')
       .waitForElementVisible('[data-id="movePluginToRight"]')
       .assert.containsText('.sidepanel h6[data-id="sidePanelSwapitTitle"]', 'SOLIDITY COMPILER')
-      .waitForElementNotVisible('.right-side-panel h6[data-id="sidePanelSwapitTitle"]')
+      .waitForElementVisible('*[data-id="close_settings"]')
+      .click('*[data-id="close_settings"]')
   },
   'Should pin a plugin while an another plugin is already pinned': function (browser: NightwatchBrowser) {
     browser.waitForElementVisible('[data-id="movePluginToRight"]')
